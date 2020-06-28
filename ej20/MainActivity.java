7a90f985858990a8ef4686c307489ac29c4cb8b5
Andrea Lizeth Quishpe Zapata <38335472+AndreaLizeth@users.noreply.github.com>
2020-06-28 00:00:33 -0500

    Add files via upload

---
+++ ej20/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.ej20;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    private ImageView iv1;
+    private Button b1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        iv1=(ImageView)findViewById(R.id.iv1);
+        b1=(Button)findViewById(R.id.b1);
+
+    }
+
+    public void ocultar(View view){
+        b1.setVisibility(View.INVISIBLE);
+        iv1.setVisibility(view.VISIBLE);
+    }
+}
